1cd7a4a492b2146a1181a93dd3af5bde0c56b891
Alexander Marcham <alexander.marcham@gmail.com>
2022-05-10 13:18:49 -0500

    origin

---
+++ src/features/totalTime.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components/native";
+
+export const totalTime = () => {
+  const [totalTime, setTotalTime] = useState(0);
+};
---
+++ src/services/mock/array.js
@@ -0,0 +1,74 @@
+export const sessions = [
+  {
+    project: "GermErase",
+    start: new Date("May 01, 2022 12:15:00"),
+    end: new Date("May 01, 2022 14:15:00"),
+    comment:
+      "Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
+    tags: ["test", "items"],
+  },
+  {
+    project: "Mancini",
+    start: new Date("May 02, 2022 11:45:00"),
+    end: new Date("May 02, 2022 12:15:00"),
+    comment:
+      "Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
+    tags: ["test", "items"],
+  },
+
+  {
+    project: "GermErase",
+    start: new Date("May 02, 2022 14:00:00"),
+    end: new Date("May 02, 2022 14:55:00"),
+    comment:
+      "Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
+    tags: ["test", "items"],
+  },
+  {
+    project: "Playing Games",
+    start: new Date("May 07, 2022 18:15:00"),
+    end: new Date("May 07, 2022 21:00:00"),
+    comment:
+      "Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
+    tags: ["test", "items"],
+  },
+  {
+    project: "GermErase",
+    start: new Date("May 08, 2022 12:15:00"),
+    end: new Date("May 08, 2022 14:15:00"),
+    comment:
+      "Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
+    tags: ["test", "items"],
+  },
+  {
+    project: "GermErase",
+    start: new Date("May 09, 2022 12:15:00"),
+    end: new Date("May 09, 2022 14:15:00"),
+    comment:
+      "Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim.",
+    tags: ["test", "items"],
+  },
+];
+
+export const projects = [
+  {
+    name: "GermErase",
+    category: "Work",
+    color: "#FF0000",
+    target: 36000,
+  },
+
+  {
+    name: "Mancini",
+    category: "Work",
+    color: "purple",
+    target: 12000,
+  },
+
+  {
+    name: "Playing Games",
+    category: "Work",
+    color: "green",
+    target: 0,
+  },
+];
